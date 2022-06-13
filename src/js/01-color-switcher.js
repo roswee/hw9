798f95a2fc398a77bef6ac0440e3c7e4910ab444
Paweł Rosłaniec <pawel94roslaniec@gmail.com>
2022-06-13 12:24:56 +0200

    task 1 ready ☺

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,21 @@
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
 
+const body = document.querySelector("body")
+const startBtn = document.querySelector("button[data-start]");
+const stopBtn = document.querySelector("button[data-stop]");
+let timer = null;
+function color() {
+    timer = setInterval(() => {body.style.backgroundColor = getRandomHexColor()}, 1000);
+    startBtn.setAttribute("disable", "disable")
+};
+
+function stoper() {
+    startBtn.removeAttribute("disable");
+
+};
+
+startBtn.addEventListener("click", color);
+
+stopBtn.addEventListener("click", stoper)
