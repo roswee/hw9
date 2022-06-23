01bfa4c76ef0e6be1845b98be24276f67cfa7758
Paweł Rosłaniec <pawel94roslaniec@gmail.com>
2022-06-23 17:34:24 +0200

    task 3 complete

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,36 @@
+import Notiflix from 'notiflix';
+
 function createPromise(position, delay) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
-    // Fulfill
+    resolve({ position, delay });
   } else {
-    // Reject
+    reject({ position, delay });
+  }
+})}, delay.value);
+};
+
+const form = document.querySelector('.form');
+const { delay, step, amount } = document.querySelector('form');
+let position = null;
+
+
+form.addEventListener('submit', handleEvent);
+
+function handleEvent(e) {
+  e.preventDefault();
+  for (i = 0; i < amount.value; i++) {
+    setTimeout(() => {
+      position = i + 1;
+      createPromise(position, delay)
+      .then(({ position, delay }) => {
+        Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+    }, i * step.value)
   }
 }
